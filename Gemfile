29db094d4f1e526a43337ceabd8a8f0028ca49ae
Rehan Iftikhar <rehan.iftikhar@gmail.com>
2011-11-26 15:39:53 -0800

    Refactored

--- Gemfile
+++ Gemfile
@@ -2,8 +2,8 @@ source :rubygems
 
 gem 'sinatra'
 gem 'sinatra-synchrony', :require => 'sinatra/synchrony', :path => "/home/rehanift/src/sinatra-synchrony"
-gem 'eventmachine'
+gem 'eventmachine', "1.0.0.beta.3"
 gem 'thin'
-gem 'em-zeromq', :git => "git://github.com/andrewvc/em-zeromq.git"
+gem 'em-zeromq', :path => "/home/rehanift/src/em-zeromq"
 gem 'ffi'
 gem "ffi-rzmq" #, "~> 0.8.2"
--- app.rb
+++ app.rb
@@ -3,61 +3,115 @@ require 'pp'
 require 'json'
 
 Thread.abort_on_exception = true
-$response = ""
-$paused_fibers = {}
-$num_calls = 0
+# $response = ""
+# $paused_fibers = {}
+# $num_calls = 0
+# 
+# class EMTestSubHandler
+#   attr_reader :received
+# 
+#   def on_readable(socket, messages)
+#     $num_calls = $num_calls + 1
+#     pp $paused_fibers.keys
+#     puts "# of messages: #{messages.size}"
+# 
+#     messages.each do |m|
+#       message = m.copy_out_string
+#       payload = JSON.parse(message)
+#       data = payload['data']
+#       request_id = payload['request_id']
+# 
+#       puts "message received, request id:#{request_id}, #{data}"
+#       if $paused_fibers[request_id.to_s]
+#         $paused_fibers.delete(request_id.to_s).resume data
+#       else
+#         puts "tried to resume a fiber that doesnt exist: #{request_id.to_s}"
+#       end
+#     end
+# 
+#   end
+# end
 
-class EMTestSubHandler
-  attr_reader :received
 
-  def on_readable(socket, messages)
-    $num_calls = $num_calls + 1
-    pp $paused_fibers.keys
-    puts "# of messages: #{messages.size}"
-
-    messages.each do |m|
-      message = m.copy_out_string
-      payload = JSON.parse(message)
-      data = payload['data']
-      request_id = payload['request_id']
-
-      puts "message received, request id:#{request_id}, #{data}"
-      if $paused_fibers[request_id.to_s]
-        $paused_fibers.delete(request_id.to_s).resume data
-      else
-        puts "tried to resume a fiber that doesnt exist: #{request_id.to_s}"
+module EventMachine
+  module EngineJS
+    class Handler
+      attr_reader :received
+      
+      def on_readable(socket, messages)
+        puts "Callback Fiber: #{Fiber.current.object_id}"
+        messages.each do |m|
+          message = JSON.parse(m.copy_out_string)
+          uuid = message['uuid']
+          data = message['message']
+          if Client.halted_fibers[uuid]
+            puts "halted fibers remaining: #{Client.halted_fibers.size}"
+            Client.halted_fibers.delete(uuid).resume data
+          else
+            puts "looking for missing fiber"
+            #pp Client.halted_fibers
+            #Fiber.yield
+          end
+        end
       end
     end
 
+    class Client
+      class << self
+        attr_accessor :halted_fibers
+      end
+      @halted_fibers = {}
+      
+      def initialize(uuid)
+        @uuid = uuid
+        self.class.halted_fibers[@uuid] = Fiber.current
+
+        ctx = EM::ZeroMQ::Context.new(1)
+
+        @push_socket = ctx.connect(ZMQ::PUSH, "tcp://127.0.0.1:5555")
+        @sub_socket = ctx.connect(ZMQ::SUB, 'tcp://127.0.0.1:5556', Handler.new)
+        @sub_socket.subscribe()
+
+      end
+
+      def run(message)
+        data = {
+          :uuid => @uuid,
+          :message => message
+        }
+        @push_socket.send_msg(data.to_json)
+        puts "message sent"
+        response = Fiber.yield
+        puts "after yield"
+        yield response
+      end
+
+      def close
+        @sub_socket.unbind
+        @push_socket.unbind
+      end
+
+    end
   end
 end
 
+
 class App < Sinatra::Base
   register Sinatra::Synchrony
+  
+  puts "Root Fiber: #{Fiber.current.object_id}"
+
   get '/' do
-    puts "request received: #{request.object_id}"
-    ctx = EM::ZeroMQ::Context.new(1)
+    puts "Request Fiber: #{Fiber.current.object_id}, Request ID: #{request.object_id.to_s}"
+    message = nil
+    client = EventMachine::EngineJS::Client.new(request.object_id.to_s)    
+    client.run("Hello World #{Time.now}") do |eval|
+      puts "Done"
+      message = eval
+    end
 
-    sub_socket = ctx.connect(ZMQ::SUB, 'tcp://127.0.0.1:5556', EMTestSubHandler.new)
-    sub_socket.subscribe()
+    client.close
 
-    push_socket = ctx.connect(ZMQ::PUSH, "tcp://127.0.0.1:5555")
-
-    message = {
-      :data => "Hello World #{Time.now}",
-      :request_id => request.object_id
-    }
-    push_socket.send_msg(message.to_json)
-
-    $paused_fibers[request.object_id.to_s] = Fiber.current
-    puts "before yield, request id: #{request.object_id}"
-    message = Fiber.yield
-    puts "after yield, request id: #{request.object_id}"
-
-    sub_socket.unbind
-    push_socket.unbind
-
-    puts "done, request id: #{request.object_id}, num_calls: #{$num_calls}"
     message
   end
 end
